632917294e6e4087ca8f1ae344c52fea1621e7e9
joekor463 <63947827+joekor463@users.noreply.github.com>
2020-05-08 08:06:04 +0800

    Add files via upload

---
+++ ComplexNumbers.java
@@ -0,0 +1,24 @@
+public class ComplexNumbers {
+    private double real;
+    double imaginary;
+
+    public ComplexNumbers(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal{
+    double real1 = real;
+    return real1;
+    }
+
+    public double getImaginary {
+        double im = imaginary;
+        return im;
+    }
+
+    public double add(double real, double imaginary){
+            
+    }
+
+}
